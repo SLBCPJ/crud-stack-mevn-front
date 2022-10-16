aa6c861192f69c96d85863a905db797c458b1b8b
SLBCPJ <jiimmypedrazz@gmail.com>
2022-10-15 19:41:06 -0500

    initial frontend

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,16 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe('My First Test', () => {
+/* describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js App')
+  })
+}) */
+describe('Test links',()=>{
+  beforeEach(()=>{
+    cy.visit('http://localhost:8080')
+  })
+  it('check links',()=>{
+    cy.get('[data-cy=links]').children().should('have.length',2)
   })
 })
